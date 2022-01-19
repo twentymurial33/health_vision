95d603d5f90de1cc65f36e609bb0df7b80cacca1
Murial Anindo <murial.anindo@lonett.com>
2022-01-19 13:36:37 -0500

    added homepage

--- vision/src/App.js
+++ vision/src/App.js
@@ -1,12 +1,13 @@
-import SignUp from "./components/SignUp";
+// import SignUp from "./components/SignUp";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <SignUp />
-      {/* <HomePage /> */}
+      {/* <SignUp /> */}
+      <HomePage />
     </div>
   );
 }
--- vision/src/components/HomePage.jsx
+++ vision/src/components/HomePage.jsx
@@ -1,27 +1,45 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import RestoreIcon from "@mui/icons-material/Restore";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 export default function HomePage() {
   const [value, setValue] = React.useState(0);
-
   return (
-    <Box sx={{ width: 500 }}>
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      >
-        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
-      </BottomNavigation>
-    </Box>
+    <>
+      <Card sx={{ maxWidth: 345 }} className="card_name">
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+          <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        </CardActions>
+      </Card>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+          <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        </CardActions>
+      </Card>
+    </>
   );
 }

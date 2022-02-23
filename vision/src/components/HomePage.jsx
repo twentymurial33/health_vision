fbd5e0afbe179336faec5c7f5048133cc28b29fe
Murial Anindo <murial.anindo@lonett.com>
2022-02-22 19:10:05 -0500

    style change

--- vision/src/components/HomePage.jsx
+++ vision/src/components/HomePage.jsx
@@ -82,7 +82,7 @@ export default function HomePage(props) {
             sx={{
               width: "100%",
               height: 400,
-              maxWidth: 360,
+              maxWidth: 380,
               bgcolor: "background.paper",
             }}
           >
